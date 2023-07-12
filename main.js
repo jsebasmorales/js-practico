021353480886854a03fc1e94d8d3f31c574ee791
Sebastian Morales <juan.sebastianmsalazar@gmail.com>
2023-07-11 21:34:12 -0500

    Add Products en index

--- main.js
+++ main.js
@@ -1,8 +1,106 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarrioIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarrioIcon.addEventListener("click", toggleCarritoAside);
+
+// Toggle Menú Desktop
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle("inactive");
 }
+
+// Toggle Menú Mobile
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
+}
+
+// Toggle Carrito de Compras
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed || !isDesktopMenuClosed) {
+        mobileMenu.classList.add("inactive");
+        desktopMenu.classList.add("inactive");
+    }
+
+    aside.classList.toggle("inactive");
+}
+
+// PRODUCTOS
+
+const productList = [];
+
+productList.push({
+    name: "Chair",
+    price: 90,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Socks",
+    price: 80,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Laptop",
+    price: 1200,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+
+        const productInfoDiv = document.createElement("div");
+
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement("figure");
+        const productImgCard = document.createElement("img");
+        productImgCard.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+        productInfoFigure.appendChild(productImgCard);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
