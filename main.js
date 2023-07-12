41af96d1afdd70210053c6c3a7831c2bf68f3c24
Sebastian Morales <juan.sebastianmsalazar@gmail.com>
2023-07-11 22:05:21 -0500

    Add Product Detail. Ajustes de CSS

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 const menuCarrioIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -14,10 +14,10 @@ menuCarrioIcon.addEventListener("click", toggleCarritoAside);
 
 // Toggle Menú Desktop
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
     if (!isAsideClosed) {
-        aside.classList.add("inactive");
+        shoppingCartContainer.classList.add("inactive");
     }
 
     desktopMenu.classList.toggle("inactive");
@@ -25,10 +25,10 @@ function toggleDesktopMenu() {
 
 // Toggle Menú Mobile
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains("inactive");
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
 
     if (!isAsideClosed) {
-        aside.classList.add("inactive");
+        shoppingCartContainer.classList.add("inactive");
     }
 
     mobileMenu.classList.toggle("inactive");
@@ -44,7 +44,7 @@ function toggleCarritoAside() {
         desktopMenu.classList.add("inactive");
     }
 
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 // PRODUCTOS
